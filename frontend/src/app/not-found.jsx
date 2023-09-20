03ba42c01b70f8223f8da07e13bfec39847dfb26
keisukearai-ht <keisuke.arai@partner.h-t.co.jp>
2023-09-20 17:30:40 +0900

    404

---
+++ frontend/src/app/not-found.jsx
@@ -0,0 +1,18 @@
+{/*
+  画面名:404 Page Not Found
+*/}
+export default function NotFound() {
+  return (
+    <>
+      <title>404 NotFound</title>
+      <main className="p-5 pt-32 mb-12 sm:ml-64">
+        <div className="mx-auto text-center text-2xl">
+          404 Not Found
+        </div>
+        <div className="mx-auto text-center text-base">
+          該当のページは存在しません。
+        </div>
+      </main>
+    </>
+  )
+}
