0e15d7db5f98d55289718ef3fca93ed4f9d93510
keisukearai <araiautocom@gmail.com>
2023-12-01 20:19:45 +0900

    sh

--- backend/tool/restart_runserver.sh
+++ backend/tool/restart_runserver.sh
@@ -1,5 +1,2 @@
-#!/bin/bash
-
-# kill & restart runserver
 ps aux | grep runserver | grep -v grep | awk '{ print "kill -9", $2 }' | sh
 /usr/bin/python /var/www/html/water_gate/backend/manage.py runserver 127.0.0.1:8000 --insecure &
