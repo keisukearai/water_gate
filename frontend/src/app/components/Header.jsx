f9d7a97a77a6228878c15fd3777673d499372047
keisukearai <araiautocom@gmail.com>
2024-01-08 16:35:16 +0900

    color

--- frontend/src/app/components/Header.jsx
+++ frontend/src/app/components/Header.jsx
@@ -9,7 +9,7 @@ export default function Header(props) {
   return (
     <header>
       <div className="z-20 w-full relative">
-        <div className="fixed top-0 left-0 flex h-20 w-full text-header-text-color border-b-2 border-neutral-300 bg-gradient-to-r from-cyan-900 to-cyan-700">
+        <div className="fixed top-0 left-0 flex h-20 w-full text-header-text-color border-b-2 border-neutral-300 bg-gradient-to-r from-cyan-900 to-cyan-500">
           {/* ハンバーガーメニューボタン */}
           <HamburgerMenuButton {...props} />
           <div className="text-header-text-color text-center flex justify-center items-center px-10 text-2xl font-semibold font-mono">
